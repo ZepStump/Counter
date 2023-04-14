bc55c6147e6517eb6725de146e0e9773823e58a3
Askar Bashirov <zep_stump@berkeley.edu>
2023-04-14 14:15:52 -0700

    edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Header'>
+        <div className='Header_text'> 
+          <div> LightHall Challenge Level 1 </div>
+        </div>
+        <div className='Main_text'> 
+          <div> Click here to increase the counter </div>
+        </div>
+        <div className='counter_var'>
+          <div> Counter: {counter}</div>
+        </div>
+        <button className='button' onClick={incrementCounter}> 
+          Click me ! 
+        </button>
+      </div>
     </div>
   );
 }
