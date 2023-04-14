4cf39644249f0bd82bf61588ed1f478e9adec80b
Askar Bashirov <zep_stump@berkeley.edu>
2023-04-14 16:45:34 -0700

    donegit add .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
-import { addDoc, collection, getDoc } from "firebase/firestore"
+import { addDoc, collection, getDoc, increment } from "firebase/firestore"
 import { db } from "./firebase-setup/firebase"
+import axios from "axios";
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(-1);
+  const [state, setState] = useState("");
+  const [table, setTable] = useState([]);
 
   const incrementCounter = () => {
     setCounter(counter + 1);
   }
 
   useEffect(() => {
-    setCounter(db.collection("counter").doc('data').number);
+    db.collection("counter").doc("data")
+        .onSnapshot(doc => setCounter(doc.data().number));
+    db.collection('countries')
+        .onSnapshot(snapshot => (
+            setTable(snapshot.docs.map(doc => ({
+              id: doc.id,
+              data: doc.data()
+            })))
+        ))
+    console.log(table)
+  }, [])
+
+  useEffect(() => {
+    console.log(table)
+    console.log(state)
+    if (counter > -1 && state != "") {
+      db.collection("counter").doc("data")
+      .update({number: counter});
+      var total = 0
+      if (table) {
+        for (let i = 1; i < table.length; i++) {
+          if (table[i].id == state) {
+            total = table[i].data.number
+          }
+        }
+      }
+      console.log(total)
+      db.collection("countries").doc(state)
+      .update({number: total + 1})
+    }
   }, [counter])
+
+  const getGeoInfo = () => {
+    axios
+      .get("https://ipapi.co/json/")
+      .then((response) => {
+        let data = response.data;
+        setState(data.country_name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getGeoInfo();
+  }, []);
 
   return (
     <div className="App">
@@ -30,6 +78,15 @@ function App() {
         <button className='button' onClick={incrementCounter}> 
           Click me ! 
         </button>
+      </div>
+      <div className='Body'>
+        <div>
+          {table.map(entry =>(
+            <div className="entries">
+            <p> {entry.id} : {entry.data.number}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/firebase-setup/firebase.js
+++ src/firebase-setup/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,8 +20,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
 
 const db = app.firestore();
 
